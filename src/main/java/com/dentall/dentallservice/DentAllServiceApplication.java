41b40240daf010809cfc027478bac4989b4bcba3
Maksim <madzarmaksim@gmail.com>
2023-10-21 20:19:23 +0200

    Init

---
+++ src/main/java/com/dentall/dentallservice/DentAllServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dentall.dentallservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentAllServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentAllServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dentall/dentallservice/DentAllServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentall.dentallservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentAllServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
