07071fd6e4fd94c5140a7388ad662d989c8c8108
Maksim <madzarmaksim@gmail.com>
2023-10-31 13:55:23 +0100

    Delete unnecessary files

--- src/main/java/com/dentall/dentallservice/exception/exceptions/AccommodationNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.dentall.dentallservice.exception.exceptions;
-
-public class AccommodationNotFound extends RuntimeException {
-    public AccommodationNotFound(String id) {
-        super("Accommodation with id: '" + id + "' not found!");
-    }
-}
