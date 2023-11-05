5f63e9eb14697d2f42ce278eeb7e3d5099cc687f
Neven Lukić <neven.lukic8@gmail.com>
2023-11-05 16:12:52 +0100

    added tailwind and chakraui

---
+++ src/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,58 @@
+import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassowrd, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShowPassword(state => !state);
+
+  return (
+    <div className="bg-gray-100 h-full p-8">
+      <Link className="text-cyan-500 text-2xl font-bold italic" to="/">
+        DentAll
+      </Link>
+      <div className="max-w-md mx-auto bg-white rounded-lg shadow-md mt-48">
+        <div className="p-8 space-y-8">
+          <p className="text-center font-bold">Prijavi se u svoj račun</p>
+          <div>
+            <p className="mb-2">Email</p>
+            <Input
+              value={email}
+              type="email"
+              onChange={e => setEmail(e.target.value)}
+              placeholder="john.doe@domain.example"
+            />
+          </div>
+          <div>
+            <p className="mb-2">Password</p>
+            <InputGroup>
+              <Input
+                value={password}
+                type={showPassowrd ? "text" : "password"}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="yourpassword"
+              />
+              <InputRightElement width="4.5rem">
+                <Button
+                  h="1.75rem"
+                  size="sm"
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassowrd ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </div>
+          <Button width={"full"} colorScheme="orange">
+            Prijavi se
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
