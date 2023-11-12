5c931cbc778ef60af75e9896ceb739e21f131b2f
rgligora <99548015+rgligora@users.noreply.github.com>
2023-11-12 22:12:13 +0100

    Added GET functionality to the Patient entity. Both GET single patient by ID and get List of all Patients.

--- src/backend/src/main/java/com/dentall/dentallservice/service/PatientService.java
+++ src/backend/src/main/java/com/dentall/dentallservice/service/PatientService.java
@@ -4,12 +4,18 @@ import com.dentall.dentallservice.model.dto.AccommodationOrderDto;
 import com.dentall.dentallservice.model.dto.PatientDto;
 import com.dentall.dentallservice.model.request.CreateAccommodationOrderRequest;
 import com.dentall.dentallservice.model.request.CreatePatientRequest;
-import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface PatientService {
+
     PatientDto createPatient(CreatePatientRequest request);
 
     void deletePatient(String id);
+
+    PatientDto retrievePatient(String id);
+
+    List<PatientDto> retrieveAllPatients();
 
     AccommodationOrderDto createAccommodationOrder(CreateAccommodationOrderRequest request);
 
