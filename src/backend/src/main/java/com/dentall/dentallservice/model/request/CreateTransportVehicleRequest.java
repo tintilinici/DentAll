d7666fb9d6e8cb41155f765aaac13362e3047e0d
Filip Buljan <filipbuljan2002@gmail.com>
2023-11-13 18:29:16 +0100

    Add transport vehicle service interface and create request class

---
+++ src/backend/src/main/java/com/dentall/dentallservice/model/request/CreateTransportVehicleRequest.java
@@ -0,0 +1,21 @@
+package com.dentall.dentallservice.model.request;
+
+import com.dentall.dentallservice.model.domain.TransportVehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTransportVehicleRequest {
+
+    private String id;
+
+    private int capacity;
+
+    private TransportVehicleType transportVehicleType;
+
+    private String transportCompanyId;
+}
