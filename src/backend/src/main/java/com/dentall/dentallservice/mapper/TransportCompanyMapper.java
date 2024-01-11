aefc4c9be7ded459a61dfcc5225bf813eff5b1fc
Maksim <madzarmaksim@gmail.com>
2024-01-11 12:26:10 +0100

    change company dto

--- src/backend/src/main/java/com/dentall/dentallservice/mapper/TransportCompanyMapper.java
+++ src/backend/src/main/java/com/dentall/dentallservice/mapper/TransportCompanyMapper.java
@@ -1,31 +1,17 @@
 package com.dentall.dentallservice.mapper;
 
 import com.dentall.dentallservice.model.domain.TransportCompany;
-import com.dentall.dentallservice.model.domain.TransportVehicle;
 import com.dentall.dentallservice.model.dto.TransportCompanyDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Mapper(componentModel = "Spring")
+@Mapper(componentModel = "Spring", uses = TransportVehicleMapper.class)
 public interface TransportCompanyMapper {
 
-    @Mapping(target = "transportVehiclesIds", source = "transportVehicles", qualifiedByName = "mapVehiclesToIds")
+    @Mapping(target = "transportVehicles", source = "transportVehicles")
     TransportCompanyDto modelToDto(TransportCompany transportCompany);
 
     List<TransportCompanyDto> modelsToDtos(List<TransportCompany> transportCompanies);
-
-    @Named("mapVehiclesToIds")
-    default List<String> mapVehiclesToIds(List<TransportVehicle> vehicles) {
-        if (vehicles == null) {
-            return Collections.emptyList();
-        }
-        return vehicles.stream()
-                .map(TransportVehicle::getId)
-                .collect(Collectors.toList());
-    }
 }
