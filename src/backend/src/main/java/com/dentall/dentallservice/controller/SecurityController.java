f2ff4aa40baa06be2e460375b59ff7d9d5b03d6d
Maksim <madzarmaksim@gmail.com>
2024-01-13 11:08:52 +0100

    remove import

--- src/backend/src/main/java/com/dentall/dentallservice/controller/SecurityController.java
+++ src/backend/src/main/java/com/dentall/dentallservice/controller/SecurityController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
