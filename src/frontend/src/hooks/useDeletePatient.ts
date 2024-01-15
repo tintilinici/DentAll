cd0fa84ad8a9eb971887bfe422fecab01cd6abf8
Karla <karla.smuk@gmail.com>
2024-01-15 10:40:19 +0100

    Updated hooks with auth

--- src/frontend/src/hooks/useDeletePatient.ts
+++ src/frontend/src/hooks/useDeletePatient.ts
@@ -1,13 +1,15 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { customFetch } from '../lib/customFetch'
+import { useAuth } from '../components/auth/useAuth'
 
 export const useDeletePatientMutation = () => {
   const queryClient = useQueryClient()
+  const { token } = useAuth()
 
   return useMutation({
     mutationKey: ['deletePatient'],
     mutationFn: (patientId: string) =>
-      customFetch(`patients/${patientId}`, { method: 'DELETE', interesedInData: false }),
+      customFetch(`/patients/${patientId}`, { method: 'DELETE', interesedInData: false }, token),
     onSuccess: () => {
       return queryClient.invalidateQueries({ queryKey: ['patients'] })
     },
--- src/frontend/src/hooks/useGetPatients.ts
+++ src/frontend/src/hooks/useGetPatients.ts
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
 import { customFetch } from '../lib/customFetch'
 import { Patient } from '../lib/api.types'
+import { useAuth } from '../components/auth/useAuth'
 
 export const useGetPatients = () => {
+  const { token } = useAuth()
+
   return useQuery({
-    queryFn: () => customFetch<Patient[]>('patients'),
+    queryFn: () => customFetch<Patient[]>('/patients', {}, token),
+    // eslint-disable-next-line @tanstack/query/exhaustive-deps
     queryKey: ['patients'],
   })
 }
